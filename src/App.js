c4f22c7c3762595c583bf8f6634fdc785bd63d7d
Dharm Solanki <dharm.solanki@drcsystems.com>
2024-07-29 18:43:10 +0530

    improve darkmode

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../custom/custom.css';
 import { Link } from 'react-router-dom';
 import { Switch } from '@mui/material';
 
-export default function Navbar() {
+export default function Navbar({onChangeX}) {
     const [isToggled, setIsToggled] = useState(false);
     const [myStyle, setMyStyle] = useState("light");
 
     const handleChange = () => {
+        const newTheme = !isToggled ? "dark" : "light";
         setIsToggled(!isToggled);
-        if (!isToggled) {
-            setMyStyle("dark")
-        } else {
-            setMyStyle("light")        
-        }
+        setMyStyle(newTheme);
+        document.body.classList.add(!isToggled ? "dark" : "light");
+        document.body.classList.remove(isToggled ? "dark" : "light");
     };
 
     return (
         <div>
             <nav className={`navbar navbar-expand-lg navbar-${myStyle} bg-${myStyle}`}>
                 <Link className="navbar-brand" to="/">TextWizard</Link>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <Link class="nav-link" to="/">Home</Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" to="/about">About Us</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About Us</Link>
                     </li>
                 </ul>
                 <Switch color="warning" onChange={handleChange} />
