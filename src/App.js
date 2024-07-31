27a029b0c413a477db10d3d079633ed7a514ef86
Dharm Solanki <dharm.solanki@drcsystems.com>
2024-07-31 18:24:20 +0530

    fix home routing path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path={"/home"} element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Signup />} />
           <Route path="/" element={<Login />} />
