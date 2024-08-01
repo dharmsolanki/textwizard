4454f0a11337a33a3aefb265c68f93261c5e6986
Dharm Solanki <dharm.solanki@drcsystems.com>
2024-08-01 11:49:06 +0530

    make proper login functionality

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,75 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Form, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
 
-    const [loginInfo, setLoginInfo] = useState({
-        email: '',
-        password: ''
-    });
-    const [loggedIn, setLoggedIn] = useState(false);
-    const navigate = useNavigate();
-    const usersData = JSON.parse(localStorage.getItem('formData'));
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
+  const usersData = JSON.parse(localStorage.getItem("formData"));
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setLoginInfo((prevLoginInfo) => ({
-            ...prevLoginInfo,
-            [name]: value
-        }));
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setLoginInfo((prevLoginInfo) => ({
+      ...prevLoginInfo,
+      [name]: value,
+    }));
+  };
 
-    const compareSingleToMultiple = (singleObj, multipleObjs) => {
-        return multipleObjs.filter(obj =>
-            Object.keys(singleObj).every(key => singleObj[key] === obj[key])
-        );
+  const compareSingleToMultiple = (singleObj, multipleObjs) => {
+    return multipleObjs?.filter((obj) =>
+      Object.keys(singleObj).every((key) => singleObj[key] === obj[key]),
+    );
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const matchingObjects = compareSingleToMultiple(loginInfo, usersData);
+    if (matchingObjects.length == 1) {
+      setLoggedIn(true);
+      navigate("/home");
+      localStorage.setItem("isLoggedIn", true);
+    } else {
+      setLoggedIn(false);
+      toast.error("Invalid Credentials.");
     }
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const matchingObjects = compareSingleToMultiple(loginInfo, usersData);
-        if (matchingObjects.length == 1) {
-            setLoggedIn(true);
-            navigate('/home');
-            localStorage.setItem('loggedIn',true);
-        }
-        else {
-            setLoggedIn(false);
-            toast.error('Invalid Credentials.')
-        }
+  useEffect(() => {
+    setLoggedIn(localStorage.getItem("isLoggedIn") ? true : false);
+  }, []);
+
+  useEffect(() => {
+    if (loggedIn) {
+      toast.success("Login Success.");
+      navigate("/home");
     }
+  }, [loggedIn]);
 
-    useEffect(() => {
-        if (loggedIn) {
-            toast.success('Login Success.')
-        }
-    }, [loggedIn]);
+  return (
+    <div className="container my-5">
+      <h2 className="text-center mb-4">Login</h2>
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={loginInfo.email}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-    return (
-        <div className='container my-5'>
-            <h2 className="text-center mb-4">Login</h2>
-            <Row className="justify-content-center">
-                <Col md={6}>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Enter email" name='email' value={loginInfo.email} onChange={handleChange} />
-                        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={loginInfo.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" name='password' value={loginInfo.password} onChange={handleChange} />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit" className="w-100" onClick={handleLogin}>
-                            Login
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-            <ToastContainer />
-        </div>
-    )
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-100"
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <ToastContainer />
+    </div>
+  );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,58 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import '../custom/custom.css';
-import { Link } from 'react-router-dom';
-import { Switch } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import "../custom/custom.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Switch } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
-    const [checked, setChecked] = useState(false);
-    const newTheme = checked ? "dark" : "light";
+  const navigate = useNavigate();
 
-    const handleChange = () => {
-        setChecked(!checked);
-        window.localStorage.setItem('setToggle',!checked);
-        document.body.classList.add(!checked ? "dark" : "light");
-        document.body.classList.remove(checked ? "dark" : "light");
-    };
+  const [checked, setChecked] = useState(false);
+  const newTheme = checked ? "dark" : "light";
 
-    useEffect(() => {
-        const getLocalStorage = window.localStorage.getItem('setToggle');
-        if(getLocalStorage === 'true') {
-            setChecked(true);
-            document.body.classList.add('dark');
-            document.body.classList.remove('light');
-        }
-        else {
-            setChecked(false);
-            document.body.classList.add('light');
-            document.body.classList.remove('dark');
-        }
-    },[]);
+  const handleChange = () => {
+    setChecked(!checked);
+    window.localStorage.setItem("setToggle", !checked);
+    document.body.classList.add(!checked ? "dark" : "light");
+    document.body.classList.remove(checked ? "dark" : "light");
+  };
 
-    return (
-        <div>
-            <nav className={`navbar navbar-expand-lg navbar-${newTheme} bg-${newTheme}`}>
-                <Link className="navbar-brand" to="/">TextWizard</Link>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/home">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/about">About Us</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Sign Up</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Login</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Switch color="warning" onChange={handleChange} checked={checked}/>
-                        <FontAwesomeIcon icon={!checked ? faMoon : faSun} />
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const getLocalStorage = window.localStorage.getItem("setToggle");
+    if (getLocalStorage === "true") {
+      setChecked(true);
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      setChecked(false);
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  }, []);
+
+  return (
+    <div>
+      <nav
+        className={`navbar navbar-expand-lg navbar-${newTheme} bg-${newTheme}`}
+      >
+        <Link className="navbar-brand" to="/">
+          TextWizard
+        </Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/home">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About Us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            {localStorage.getItem("isLoggedIn") ? (
+              <Link className="nav-link" to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            ) : (
+              <Link className="nav-link" to="/">
+                Login
+              </Link>
+            )}
+          </li>
+          <li className="nav-item">
+            <Switch color="warning" onChange={handleChange} checked={checked} />
+            <FontAwesomeIcon icon={!checked ? faMoon : faSun} />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
